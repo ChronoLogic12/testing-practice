828295df0cd026c0e023be5c9a4ee69ab696d3d5
ChronoLogic <msuttie12@hotmail.com>
2022-05-05 15:28:25 +0100

    create aditional tests for addition function

--- scripts/tests/calc.js
+++ scripts/tests/calc.js
@@ -1,5 +1,8 @@
-const addition = () => {
-	return 42;
+const addition = (num1, num2) => {
+	if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+		throw new TypeError("Invalid data type. All input values must be integers.");
+	}
+	return num1 + num2;
 };
 
 module.exports = addition;
